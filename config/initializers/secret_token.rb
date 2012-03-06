be630e2cd50cf9f9ef4288011e5ee6583acbd562
Buzzkid <goobeyaf@yahoo.com>
2012-03-06 18:28:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiftDps::Application.config.secret_token = 'ae58be5dd523596b80e7af4aea69cca00b823eae20bea69628674ca662a1a40ce4bf838a6fc353ca9f806558082f70ff5bdac6ae6667b29abe7f3283f9074bc6'
